3469d7ab63e99d85dcfaa3e7a47149cfddfe16aa
Kacper Bielak <github@bielak2000.com>
2024-04-11 22:12:26 +0200

    Triger number 5

--- database/import/5_triggers.sql
+++ database/import/5_triggers.sql
@@ -37,3 +37,24 @@ DELIMITER ;
 -- FOR TESTING
 -- insert into szkola.Class (StartYear, Preceptor_UserID, ProfileID) values (1998, 791, 1);
 -- select * from szkola.Class where StartYear=1998;
+
+-- 5. Trigger - before update student to class - checking that class is graduate - procedure 1
+
+-- DROP TRIGGER before_student_update;
+DELIMITER $
+CREATE TRIGGER before_student_update BEFORE update on szkola.Student for each row
+	BEGIN
+		IF NEW.ClassID != OLD.ClassID THEN
+			CALL add_student_to_not_graduated_class(NEW.ClassID);
+		END IF;
+	END $
+DELIMITER ;
+
+-- FOR TESTING
+-- insert into szkola.Class (StartYear, GraduationYear, Preceptor_UserID, ProfileID) values (2007, 2020, 791, 1);
+-- select ClassID from szkola.Class where StartYear=2007;
+-- SELECT * FROM szkola.User where Email like 'kacperbielak123@o2.pl';
+-- DELETE FROM szkola.User where Email like 'kacperbielak123@o2.pl';
+-- insert into szkola.User (Name, Surname, Email, PasswordHash, Address, UserRoleID, PESEL) values ('kacper', 'bielak',
+--      	'kacperbielak123@o2.pl', '$2b$04$iQu6MkQgzjJTGd6YnCKfDuW/ag.Ewrr3XQE8c5hU14Io68E5UyEQ.', 'Dzwola 21', 1, '12345678911');
+-- update szkola.Student set ClassID=37 where UserID=808;
