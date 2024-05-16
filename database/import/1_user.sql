8872ec0e4cf782278f00dbceaba3017f3d5f2508
Kacper Bielak <github@bielak2000.com>
2024-05-16 17:16:59 +0200

    Handle polish characters

--- database/import/1_user.sql
+++ database/import/1_user.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE if not exists szkola character set UTF8MB4 collate utf8mb4_bin;
+CREATE DATABASE if not exists szkola CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 create user if not exists 'application' identified with mysql_native_password by '7966e4d8-cb1b-548a-859b-b8e336f9fcae';
 GRANT SELECT, UPDATE, INSERT, DELETE, EXECUTE ON szkola.* TO application;
